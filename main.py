68d0282c980af5d9dae52db985637dea32bb6a26
pratikd481 <pratikd481@gmail.com>
2026-03-31 08:52:31 +0530

    Faiss setup

---
+++ main.py
@@ -0,0 +1,105 @@
+import numpy as np
+import faiss
+import os
+import time
+
+# -----------------------------
+# PATHS
+# -----------------------------
+DATA_PATH = "data/vectors.npy"
+INDEX_PATH = "index/faiss.index"
+
+
+def generateVectors(vector_size=1000, num_vectors=128):
+    """
+    Generates random vectors and saves them to a file.
+    
+    Parameters:
+    vector_size (int): The size of each vector.
+    num_vectors (int): The number of vectors to generate.
+    """
+    vectors = np.random.rand(num_vectors, vector_size).astype("float32")
+    os.makedirs("data", exist_ok=True)
+    np.save(DATA_PATH, vectors)
+    print(f"Generated {num_vectors} vectors of size {vector_size} and saved to {DATA_PATH}")
+    
+    
+# -----------------------------
+# STEP 2: LOAD VECTORS
+# -----------------------------
+def load_vectors():
+    vectors = np.load(DATA_PATH)
+    print(f"[✔] Loaded vectors: {vectors.shape}")
+    return vectors
+
+
+# -----------------------------
+# STEP 3: BUILD INDEX
+# -----------------------------
+
+def build_index(vectors):
+    dim = vectors.shape[1]
+    base_index  = faiss.IndexFlatL2(dim)
+    index = faiss.IndexIDMap(base_index)
+    
+    ids = np.arange(len(vectors))
+    index.add_with_ids(vectors, ids)
+    
+    os.makedirs("index", exist_ok=True)
+    faiss.write_index(index, INDEX_PATH)
+    print(f"[✔] Index built with {index.ntotal} vectors")
+    
+
+# -----------------------------
+# STEP 4: LOAD INDEX
+# -----------------------------
+def load_index():
+    index = faiss.read_index(INDEX_PATH)
+    print(f"[✔] Index loaded with {index.ntotal} vectors")
+    return index
+
+# -----------------------------
+# STEP 5: SEARCH
+# -----------------------------
+def search(index, query_vector, k=5):
+    distances, indices = index.search(query_vector, k)
+    return distances, indices
+
+
+# -----------------------------
+# STEP 6: Timed Search
+# -----------------------------
+def timed_search(index, query, k=5):
+    start = time.time()
+    D, I = index.search(query, k)
+    end = time.time()
+
+    print(f"Search time: {(end - start)*1000:.2f} ms")
+    return D, I
+
+# -----------------------------
+# MAIN FLOW
+# -----------------------------
+if __name__ == "__main__":
+
+    # ⚠️ RUN ONLY FIRST TIME
+    # generate_vectors()
+
+    # ⚠️ RUN ONLY FIRST TIME
+    # vectors = load_vectors()
+    # build_index(vectors)
+
+    # NORMAL FLOW (AFTER SETUP)
+    vectors = load_vectors()
+    index = load_index()
+
+    # Take first vector as query
+    query = vectors[0].reshape(1, -1)
+
+    D, I = search(index, query)
+
+    print("\n🔍 Query Results")
+    print("Indices:", I)
+    print("Distances:", D)
+    print("Total vectors:", index.ntotal)
+    print("Vector dimension:", vectors.shape[1])
